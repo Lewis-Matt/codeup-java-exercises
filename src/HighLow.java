149d979b68632c22e61652b12cb3248f76fb1015
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-18 12:01:34 -0600

    added highlow game

---
+++ src/HighLow.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+public class HighLow {
+    // TODO:
+    //    Game picks a random number between 1 and 100.
+    //    Prompts user to guess the number.
+    //    All user inputs are validated.
+    //    If user's guess is less than the number, it outputs "HIGHER".
+    //    If user's guess is more than the number, it outputs "LOWER".
+    //    If a user guesses the number, the game should declare "GOOD GUESS!"
+    //    Keep track of how many guesses a user makes.
+    //    Set an upper limit on the number of guesses.
+
+
+
+}
