1bad9c2d4548b26c8eaa8c92a1f1068f9edf7353
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-18 11:13:31 -0600

    Recursive factorial

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -52,13 +52,41 @@ public class MethodsExercises {
         Scanner scanner = new Scanner(System.in);
         int userNumber = scanner.nextInt();
         while (userNumber > max || userNumber < min) {
-            System.out.println("Number must be between 1 and 10:");
+            System.out.printf("Number must be between %s and %s: ", min, max);
             return getInteger(min, max);
         }
-        System.out.printf("Success! %s is between %s and %s!", userNumber, min, max);
+        System.out.printf("Success! %s is between %s and %s!%n", userNumber, min, max);
         return userNumber;
     }
 
+    // TODO: Factorial!
+    public static long callFactorial() {
+        // Not needed, I already wrote getInteger():
+//        System.out.println("Enter a number between 1 and 10: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int userInput = scanner.nextInt();
+//        while (userInput > 10 || userInput < 1) {
+//            System.out.println("Number must be between 1 and 10: ");
+//            return callFactorial();
+//        }
+        int userInput = getInteger(1, 10);
+        System.out.printf("The factorial of %s is: %s.%n", userInput, factorial(userInput));
+        System.out.println("Would you like to continue? [y/n] ");
+        Scanner scanner = new Scanner(System.in);
+        String response = scanner.next();
+        if (response.equalsIgnoreCase("y")) {
+            callFactorial();
+        }
+        return factorial(userInput);
+    }
+
+    // Factorial calculation
+    public static long factorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return factorial(num - 1) * num;
+    }
 
     public static void main(String[] args) {
         // Add
@@ -71,10 +99,8 @@ public class MethodsExercises {
         System.out.println(divide(3, 0));
         // Modulus
         System.out.println(modulo(30, 9));
-
-        // Is number in range (min, max)
-        getInteger(1, 10);
-
+        // Factorial
+        callFactorial();
 
     }
 }
