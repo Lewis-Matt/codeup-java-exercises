01d70d03447aa0ae27cf1c8f0db414b102ead70e
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-22 12:04:34 -0600

    created util package with input and inputtest class

---
+++ src/util/Input.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.util.Scanner;
+
+// A package is a namespace that organizes a set of related classes and interfaces. Basically a directory of all the classes for an application.
+// The Java platform provides an enormous class library (a set of packages) suitable for use in your own applications. This library is known as the "Application Programming Interface", or "API" for short.
+public class Input {
+    private Scanner scanner = new Scanner(System.in);
+    public String getString() {
+
+    }
+    public boolean yesNo() {
+
+    }
+
+    public int getInt(int min, int max) {
+
+    }
+
+    public int getInt(){
+
+    }
+
+    double getDouble(double min, double max) {
+
+    }
+
+    double getDouble() {
+
+    }
+}
