9656654c701a80381296aa4cbe7f52b210cfb672
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-23 12:49:40 -0600

    finished exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,19 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
+    public static String[] adjectives = {"adorable", "adventurous", "aggressive", "agreeable", "alert", "alive", "amused", "angry", "annoyed", "anxious"};
+    public static String[] nouns = {"dog", "cat", "Nebraska", "cup", "pc", "stapler", "french toast", "bus", "Paris", "duck"};
+
+    // TODO: Create a method that will return a random element from an array of strings.
+    public static String getRandomElement(String[] arr) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(10);
+        return arr[randomIndex];
+    }
+
+    //////////////////////////////////////MAIN//////////////////////////////////////
+    public static void main(String[] args) {
+        // TODO: Select a random noun and adjective and hyphenate the combination and display the generated name to the user.
+        System.out.printf("Here is your server name:%n%S-%S", getRandomElement(adjectives), getRandomElement(nouns));
+    }
 }
