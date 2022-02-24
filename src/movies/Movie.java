196bcb98eb0b9035a2622cff58f31e10c1aab6b9
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-24 09:05:01 -0600

    movie class - constructor - getters/setters

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,36 @@
 package movies;
 
+// TODO: It should have private fields for name and category, and a constructor that sets both of these. Create methods to access these properties and change them (getters and setters).
 public class Movie {
+    private String name;
+    private String category;
+
+    // CONSTRUCTOR
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    // GETTERS
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    // SETTERS
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setCategory(String newCategory) {
+        this.category = newCategory;
+    }
+
+    ////////////////////////////////////MAIN//////////////////////////////////
+    public static void main(String[] args) {
+
+    }
 }
