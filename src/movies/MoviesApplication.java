89d3be777fb90c1939fdb178bd40fa87365db194
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-24 09:16:13 -0600

    Setup main, notes

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,4 +1,15 @@
 package movies;
 
+// TODO: Give the user a list of options for viewing all the movies or viewing movies by category.
+//  Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
+//  If a category is selected, only movies from that category should be displayed.
+//  Your application should continue to run until the user chooses to exit.
+//  Add functionality to allow a user to add a new movie to the list.
+
 public class MoviesApplication {
+    ///////////////////////////MAIN///////////////////////////
+    public static void main(String[] args) {
+
+    }
+
 }
