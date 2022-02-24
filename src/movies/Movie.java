d85ff8f257da70ed37a916d48e0a67650d66f1c9
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-24 09:06:20 -0600

    movie class - constructor - getters/setters

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -28,9 +28,4 @@ public class Movie {
     public void setCategory(String newCategory) {
         this.category = newCategory;
     }
-
-    ////////////////////////////////////MAIN//////////////////////////////////
-    public static void main(String[] args) {
-
-    }
 }
