12722655633b1e5b44fd326ae540a5f18441667e
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-03 10:27:51 -0600

    clarifying comment

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -7,7 +7,7 @@ public class ShapesTest {
         Measurable testShape2 = new Rectangle(3, 8);
         System.out.printf("testShape1 area: %s, perimeter: %s%n", testShape1.getArea(), testShape1.getPerimeter());
         System.out.printf("testShape2 area: %s, perimeter: %s%n", testShape2.getArea(), testShape2.getPerimeter());
-
+        // If I tried to do testShape1.getLength() it will not work as Measurable does not have that method, EVEN though square extends quadrilateral (which does have that method)
 
         // TODO: Create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5
         Rectangle box1 = new Rectangle(5, 4);
